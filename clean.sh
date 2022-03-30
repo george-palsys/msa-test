8aaa1d4d4882afb198d2df9bb31159867163b412
georgechiu <georgechiu@palsys.com.tw>
2022-03-29 22:10:23 -0400

    push1

--- clean.sh
+++ clean.sh
@@ -5,3 +5,8 @@ oc delete route msa-test-git
 oc delete bc msa-test-git
 oc delete secret msa-test-git-generic-webhook-secret
 oc delete secret msa-test-git-github-webhook-secret
+oc delete secret postgres-database
+oc delete svc postgres-database
+oc delete dc postgres-database
+oc delete pvc postgres-database
+
